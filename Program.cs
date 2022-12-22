cef2cdd0bed41005ba9fd80f8733506a59c4f7da
TortoiseShell04 <cutiecatdaog@gmail.com>
2022-12-22 15:34:14 +0200

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -12,11 +12,16 @@ namespace App
             int length = words.Length;
             int num = ran.Next(length);
 
-            string WordToBeGuessed = words[num];
+            string? WordToBeGuessed = words[num];
 
             int wordLength = WordToBeGuessed.Length;
             int moveCount = wordLength + 5;
-            string wordText = "";
+            string? wordText = "";
+            bool letterFound = false;
+            bool gameWon = false;
+            
+            Console.WriteLine($"HangMan by ToirtoiseShell");
+            Console.WriteLine($"-------------------------------");
             
 
             for (int i = 0; i < wordLength; i++)
@@ -28,8 +33,9 @@ namespace App
             
             while (moveCount > 0)
             {
+                letterFound = false;
                 Console.WriteLine($"Enter a letter: ");
-                string guess = Console.ReadLine();
+                string? guess = Console.ReadLine();
 
                 for (int i = 0; i < wordLength; i++)
                 {
@@ -41,21 +47,31 @@ namespace App
                             if (index == i1)
                             {
                                 strB[index] = WordToBeGuessed[i];
+                                letterFound = true;
                             }
                         
                         }
                     }
-                    else if (wordLength - i == 1)
+                    else if (wordLength - i == 1 && !letterFound)
                     {
                         moveCount--;
                     }
                 }
                 Console.WriteLine($"{strB} \t Moves left: {moveCount}");
+
+                if (strB.ToString() == WordToBeGuessed)
+                {
+                    Console.WriteLine($"You won!!!!");
+                    gameWon = true;
+                    break;
+                }
             
             }
 
-            Console.WriteLine(words[num]);
-            
+            if (!gameWon)
+            {
+                Console.WriteLine(words[num]);
+            }
         }
     }
 }
